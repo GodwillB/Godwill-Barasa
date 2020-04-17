e921b0cc202265cdac9a114ff48380e5e53af1e0
GodwillB <iamgodwillb@gmail.com>
2020-04-17 05:21:18 +0300

    fancy menu

--- js/custom.js
+++ js/custom.js
@@ -1,81 +1,21 @@
-$(document).ready(function ($) { 
+ $(window).on('load', function() {
+    $('#preloader').delay(450).fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(45050).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(45050).css({'overflow':'visible'});
+});
 
-$("#carousel-team").owlCarousel({
-        items:3,
-        loop:true,
-        nav:true,
-        center:true,
-        margin: 50,
-        URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'URLHash',
-         responsive:
-        {
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
+const elButton = document.querySelector('.menu-button');
+const elSite = document.querySelector('#site');
 
-$("#brand").owlCarousel({
-        items:3,
-        loop:true,
-        nav:true,
-        center:true,
-        margin: 50,
-        URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'URLHash',
-         responsive:
-        {
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    })
-
-$('.owl-carousel').owlCarousel({
-        items:3,
-        loop:true,
-        nav:true,
-        center:true,
-        margin:10,
-        URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'URLHash',
-         responsive:
-        {
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
-
+elButton.addEventListener('click',()=>{
+  if ( elSite.dataset.menu ) {
+    delete elSite.dataset.menu;
+  } else {
+    elSite.dataset.menu = true;
+  }
 });
 
 
-
-
-        $(window).on('load', function() {
-          $('#preloader').delay(450).fadeOut(); // will first fade out the loading animation 
-          $('#preloader').delay(45050).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-          $('body').delay(45050).css({'overflow':'visible'});
-        });
+setTimeout(()=>{ 
+  elButton.click(); 
+},100);
