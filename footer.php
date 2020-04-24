ff146593b467515d4f80a52854bca9ce8298075e
GodwillB <iamgodwillb@gmail.com>
2020-04-24 17:16:39 +0300

    file services

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script src="js/custom.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,900" rel="stylesheet">
+  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.jpg">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/fancymenu.css">
+  <link rel="stylesheet" href="css/responsiveness.css">
+  <title> Godwill Barasa</title>
+  <meta name="description" content="Godwill Barasa is an experienced rockstar designer and developer based in Nairobi, Kenya. He has worked with many big brands including Legibra, Double Shasa Limited , Kenya Wildlife Service, Catholic Church. This is a curation of his articles including tutorials and posts about user design and front-end development"/>
+</head>
+
+<body>
