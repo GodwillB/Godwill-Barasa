f0af68eeec26ecc467634172da6814cf50998b82
GodwillB <iamgodwillb@gmail.com>
2020-04-24 17:21:54 +0300

    menu services

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,19 @@
- $(window).on('load', function() {
-    $('#preloader').delay(450).fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(45050).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(45050).css({'overflow':'visible'});
-});
+$(document).ready(function() {
 
-const elButton = document.querySelector('.menu-button');
-const elSite = document.querySelector('#site');
-
-elButton.addEventListener('click',()=>{
-  if ( elSite.dataset.menu ) {
-    delete elSite.dataset.menu;
-  } else {
-    elSite.dataset.menu = true;
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
   }
+
+  $(".button").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
 });
-
-
-setTimeout(()=>{ 
-  elButton.click(); 
-},100);
